572f3bf7d9c09cc5909326653648d713110b54af
abhijeetckar92 <ckr.abhi@gmail.com>
2019-05-01 19:25:36 +0530

    Objects in Go

---
+++ 08_maps/main.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	//Define map
+	// emails := make(map[string]string)
+
+	// emails["Bob"] = "bob@gmail.com"
+	// emails["Sharon"] = "sharon@gmail.com"
+	// emails["Mike"] = "mike@gmail.com"	
+	
+	// fmt.Println(emails["Bob"])
+
+	//Declare map and add key values
+
+	emails := map[string]string{"Bob":"bob@gmail.com","Sharon":"sharon@gmail.com"}
+
+	emails["Mike"] = "mike@gmail.com"
+	fmt.Println(emails)
+	fmt.Println(len(emails))
+
+
+	//Delete from map
+	delete(emails,"Bob")
+	fmt.Println(emails)
+}
